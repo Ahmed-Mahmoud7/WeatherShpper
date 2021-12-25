53d8e1d76bc31f7a6fd6b3bce0517d9f16be1694
AhmedMahmoud <ahmed.mustafa@derayah.com>
2021-12-25 19:09:47 +0200

    Add new test cases

--- src/test/java/tests/MoisturizersTest.java
+++ src/test/java/tests/MoisturizersTest.java
@@ -1,6 +1,19 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pages.BasePage;
+import pages.CurrentTemp;
+import pages.Moisturizers;
 
 public class MoisturizersTest extends BasePage {
+
+
+    @Test(priority=0)
+    public void validateCurrentTempPageTitle() {
+
+         Moisturizers  moist = new Moisturizers(driver);
+         Assert.assertEquals(moist.getTitle(), "");
+         System.out.println("Test Run Success");
+    }
 }
